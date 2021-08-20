ca34a27bdc944c5c5df2be1e7917fe825ab55e75
magluar <aryann.magluyan@oocl.com>
2021-08-20 18:30:10 +0800

    [PosMachine][magluar] feat: getItems

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,31 @@
 package pos.machine;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
+    }
+
+    private List<Item> getItems(List<String> barcodes) {
+        List<ItemInfo> itemInfos = ItemDataLoader.loadAllItemInfos();
+        List<Item> items = new ArrayList<>();
+        List<String> distinct = barcodes.stream()
+                                .distinct()
+                                .collect(Collectors.toList());
+        for (String distinctItem : distinct) {
+            for (ItemInfo itemInfo: itemInfos) {
+                if (itemInfo.getBarcode().equals(distinctItem)){
+                    items.add(new Item(itemInfo.getName(),
+                            Collections.frequency(barcodes, distinctItem),
+                            itemInfo.getPrice(),
+                            Collections.frequency(barcodes, distinctItem) * itemInfo.getPrice()));
+                }
+            }
+        }
+        return items;
     }
 }
